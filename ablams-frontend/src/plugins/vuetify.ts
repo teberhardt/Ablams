d550a29ad940437e300f4868e3ab8b1d40524b35
teberhardt <teberhardt@mail.de>
2019-11-17 02:04:53 +0100

    vue add vuetify

--- ablams-frontend/src/plugins/vuetify.ts
+++ ablams-frontend/src/plugins/vuetify.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import de from 'vuetify/src/locale/de';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    lang: {
+      locales: { de },
+      current: 'de',
+    },
   icons: {
     iconfont: 'mdi',
   },

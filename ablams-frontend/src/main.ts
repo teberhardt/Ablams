1f6e33d29a314c35d68d2f354480f36061244d90
teberhardt <teberhardt@mail.de>
2019-11-17 01:06:53 +0100

    removing hello world component

--- ablams-frontend/src/main.ts
+++ ablams-frontend/src/main.ts
@@ -1,15 +1,32 @@
 import Vue from 'vue';
+
+// Sync router with store
+import { sync } from 'vuex-router-sync';
+
+// Application imports
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
-import store from './store'
+import store from './store';
+import 'vuetify/dist/vuetify.min.css';
+import axios from 'axios';
+
+Vue.prototype.$http = axios;
+
+// Sync store with router
+sync(store, router);
+
+Vue.use(
+    vuetify, {
+    iconfont: 'mdi',
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  vuetify,
   store,
+  vuetify,
   render: (h) => h(App)
 }).$mount('#app');
--- ablams-frontend/src/views/Home.vue
+++ ablams-frontend/src/views/Home.vue
@@ -4,11 +4,9 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default Vue.extend({
   components: {
-    HelloWorld,
   },
 });
 </script>

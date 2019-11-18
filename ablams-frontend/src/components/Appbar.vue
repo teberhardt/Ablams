fa88d7e22e07604ac6b7f5dfa06978f2346ff890
teberhardt <teberhardt@mail.de>
2019-11-18 17:37:13 +0100

    #9 add basic menu, inspired from official vuetify examples

---
+++ ablams-frontend/src/components/Appbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app-bar
+        app
+    >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>{{title  }}</v-toolbar-title>
+        <v-spacer/>
+        <Searchfield></Searchfield>
+        <v-spacer/>
+        <v-btn icon>
+            <v-icon>mdi-account-settings</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import Searchfield from '@/components/Searchfield.vue';
+
+    export default Vue.extend({
+        name: 'Appbar',
+        components: {
+            Searchfield
+        },
+        data: () => ({
+            title:"Ablams"
+        }),
+        methods: {
+        },
+    });
+</script>
+
+<style>
+
+</style>
---
+++ ablams-frontend/src/components/Searchfield.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-autocomplete prepend-inner-icon="mdi-magnify"
+                    single-line
+                    clearable
+                    filled
+                    dense
+                    hide-details
+                    rounded
+                    placeholder="Search..."
+    />
+</template>
+
+<script>
+    import { Component, Vue } from 'vue-property-decorator';
+
+    export default Vue.extend({
+        name: 'Searchfield',
+        components: {
+
+        },
+        data: () => ({
+        }),
+        methods: {
+        },
+    });
+</script>
+
+<style scoped>
+
+</style>

9dbeee5e5c0cefabd9cdcfe2be828d34ef527a20
teberhardt <teberhardt@mail.de>
2019-12-13 15:30:49 +0100

    resolving some tslint warnings in ablams-frontend

--- ablams-frontend/src/rest/AbstractRestResource.ts
+++ ablams-frontend/src/rest/AbstractRestResource.ts
@@ -1,11 +1,10 @@
-import axios, {AxiosResponse} from "axios";
+import axios, {AxiosResponse} from 'axios';
 
-export abstract class AbstractRestResource<T>{
+export abstract class AbstractRestResource<T> {
 
     private readonly ENDPOINT_URL: string;
 
-    protected constructor(endpointUrl:string)
-    {
+    protected constructor(endpointUrl: string) {
         this.ENDPOINT_URL = endpointUrl;
     }
 
@@ -13,8 +12,7 @@ export abstract class AbstractRestResource<T>{
         return axios.post(this.ENDPOINT_URL, newResource);
     }
 
-    public fetchAll(): Promise<AxiosResponse<T[]>>
-    {
+    public fetchAll(): Promise<AxiosResponse<T[]>> {
         return axios.get(this.ENDPOINT_URL);
     }
 }

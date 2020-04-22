36d0ef73c7c563443571fdf9b33be0cd7e9b0c05
teberhardt <teberhardt@mail.de>
2020-04-22 06:08:33 +0200

    introducing CoverResource.ts

---
+++ ablams-frontend/src/rest/CoverResource.ts
@@ -0,0 +1,14 @@
+import {AbstractRestResource} from "@/rest/AbstractRestResource";
+import {CoverDTO} from 'ablams-js-dto/src/domain/models';
+
+export default new class CoverResourceResource extends AbstractRestResource<CoverDTO> {
+
+    constructor() {
+        super('/api/cover');
+    }
+
+    public prepareForPost(cover: CoverDTO): CoverDTO {
+
+        return cover;
+    }
+};

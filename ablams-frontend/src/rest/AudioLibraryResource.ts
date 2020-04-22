16659a9bc8a3ec36446e89b6d23e078069289084
teberhardt <teberhardt@mail.de>
2020-04-22 06:05:35 +0200

    edit to comply with changes in parent in AudioLibraryResource.ts

--- ablams-frontend/src/rest/AudioLibraryResource.ts
+++ ablams-frontend/src/rest/AudioLibraryResource.ts
@@ -1,21 +1,15 @@
-import {AxiosResponse} from 'axios';
 import {AudioLibraryDTO} from 'ablams-js-dto/src/domain/models';
 import {AbstractRestResource} from '@/rest/AbstractRestResource';
 
 export default new class AudioLibraryResource extends AbstractRestResource<AudioLibraryDTO> {
 
-    private static prepareForPost(aLib: AudioLibraryDTO): AudioLibraryDTO {
-        // set id to undefined because a post request with an id will not be accepted by the backend
-        aLib.id = undefined;
-        return aLib;
-    }
-
     constructor() {
         super('/api/audio-libraries');
     }
 
-    public insert(newAudioLibrary: AudioLibraryDTO): Promise<AxiosResponse<AudioLibraryDTO>> {
-       newAudioLibrary = AudioLibraryResource.prepareForPost(newAudioLibrary);
-       return super.insert(newAudioLibrary);
+    public prepareForPost(aLib: AudioLibraryDTO): AudioLibraryDTO {
+        // set id to undefined because a post request with an id will not be accepted by the backend
+        aLib.id = undefined;
+        return aLib;
     }
 };

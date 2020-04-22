c9aee77af5a109faf254a7371d6b93092e4d90d3
teberhardt <teberhardt@mail.de>
2020-04-22 07:08:15 +0200

    edit consistent calling it cover in CoverResource.java

--- ablams-backend/src/main/java/de/teberhardt/ablams/web/rest/CoverResource.java
+++ ablams-backend/src/main/java/de/teberhardt/ablams/web/rest/CoverResource.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * REST controller for managing Image.
+ * REST controller for managing cover.
  */
 @RestController
 @RequestMapping("/api")
@@ -36,18 +36,18 @@ public class CoverResource {
     }
 
     /**
-     * POST  /cover : Create a new image.
+     * POST  /cover : Create a new cover.
      *
-     * @param coverDTO the imageDTO to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new imageDTO, or with status 400 (Bad Request) if the image has already an ID
+     * @param coverDTO the coverDTO to create
+     * @return the ResponseEntity with status 201 (Created) and with body the new coverDTO, or with status 400 (Bad Request) if the cover has already an ID
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PostMapping("/cover")
     @Timed
     public ResponseEntity<CoverDTO> createCover(@RequestBody CoverDTO coverDTO) throws URISyntaxException {
-        log.debug("REST request to save Image : {}", coverDTO);
+        log.debug("REST request to save cover : {}", coverDTO);
         if (coverDTO.getId() != null) {
-            throw new BadRequestAlertException("A new image cannot already have an ID", ENTITY_NAME, "idexists");
+            throw new BadRequestAlertException("A new cover cannot already have an ID", ENTITY_NAME, "idexists");
         }
         CoverDTO result = coverService.save(coverDTO);
         return ResponseEntity.created(new URI("/api/cover/" + result.getId()))
@@ -56,18 +56,18 @@ public class CoverResource {
     }
 
     /**
-     * PUT  /cover : Updates an existing image.
+     * PUT  /cover : Updates an existing cover.
      *
-     * @param coverDTO the imageDTO to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated imageDTO,
-     * or with status 400 (Bad Request) if the imageDTO is not valid,
-     * or with status 500 (Internal Server Error) if the imageDTO couldn't be updated
+     * @param coverDTO the coverDTO to update
+     * @return the ResponseEntity with status 200 (OK) and with body the updated coverDTO,
+     * or with status 400 (Bad Request) if the coverDTO is not valid,
+     * or with status 500 (Internal Server Error) if the coverDTO couldn't be updated
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PutMapping("/cover")
     @Timed
     public ResponseEntity<CoverDTO> updateCover(@RequestBody CoverDTO coverDTO) throws URISyntaxException {
-        log.debug("REST request to update Image : {}", coverDTO);
+        log.debug("REST request to update cover : {}", coverDTO);
         if (coverDTO.getId() == null) {
             throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
         }
@@ -90,29 +90,29 @@ public class CoverResource {
     }
 
     /**
-     * GET  /cover/:id : get the "id" image.
+     * GET  /cover/:id : get the "id" cover.
      *
-     * @param id the id of the imageDTO to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the imageDTO, or with status 404 (Not Found)
+     * @param id the id of the coverDTO to retrieve
+     * @return the ResponseEntity with status 200 (OK) and with body the coverDTO, or with status 404 (Not Found)
      */
     @GetMapping("/cover/{id}")
     @Timed
     public ResponseEntity<CoverDTO> getCover(@PathVariable Long id) {
-        log.debug("REST request to get Image : {}", id);
-        Optional<CoverDTO> imageDTO = coverService.findOne(id);
-        return ResponseUtil.wrapOrNotFound(imageDTO);
+        log.debug("REST request to get cover : {}", id);
+        Optional<CoverDTO> coverDTO = coverService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(coverDTO);
     }
 
     /**
-     * DELETE  /cover/:id : delete the "id" image.
+     * DELETE  /cover/:id : delete the "id" cover.
      *
-     * @param id the id of the imageDTO to delete
+     * @param id the id of the coverDTO to delete
      * @return the ResponseEntity with status 200 (OK)
      */
     @DeleteMapping("/cover/{id}")
     @Timed
     public ResponseEntity<Void> deleteCover(@PathVariable Long id) {
-        log.debug("REST request to delete Image : {}", id);
+        log.debug("REST request to delete cover : {}", id);
         coverService.delete(id);
         return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
     }

ed24d6b8b67beb097ff0e432f2d9c0c2d106bd57
teberhardt <teberhardt@mail.de>
2020-04-23 10:20:15 +0200

    first rough implementation of an audiobook overview

---
+++ ablams-frontend/src/components/AudiobookCard.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-card
+        class="ma-4"
+        hover
+        shaped
+        height="100%"
+    >
+        <v-img contain
+               align-center
+               justify-center
+               :src="getImageSrcUrl()"
+        />
+        <v-progress-linear value="10" color="green"></v-progress-linear>
+
+        <v-card-text>{{abook.name}}</v-card-text>
+
+        <v-card-actions class="card-actions" >
+            <v-btn
+                color="orange"
+                text
+            >
+                Play
+            </v-btn>
+
+            <v-btn
+                color="orange"
+                text
+            >
+                Details
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import {AudioBookDTO} from 'ablams-js-dto/src/domain/models';
+
+
+    @Component
+    export default class AudiobookCard extends Vue {
+
+        @Prop({default: 'Example'})
+       abook!: AudioBookDTO;
+
+        protected created(): void {
+            this.initialize();
+        }
+
+        protected initialize(): void {
+
+        }
+
+        public getImageSrcUrl(): string{
+            return `/api/audio-books/${this.abook.id}/cover/raw`
+        }
+
+
+    }
+</script>
+<style>
+    .card-actions {
+        position: absolute;
+        bottom: 0;
+    }
+</style>
--- ablams-frontend/src/views/AudiobooksView.vue
+++ ablams-frontend/src/views/AudiobooksView.vue
@@ -2,43 +2,26 @@
     <div>
         <v-subheader>Alle Audiobooks</v-subheader>
 
+
         <v-container fluid>
-            <v-row dense>
-                <v-col
+            <v-row justify="start">
+                <v-col cols="10"
+                       sm="6"
+                       md="3"
+                       lg="2"
+                       xl="2"
+
                     v-for="abook in aBooks"
                     :key="abook.id"
                     :cols="abook.name"
                 >
-                    <v-card
-                        class="mx-auto"
-                        max-width="400"
-                        outlined
-                    >
-                        <v-img
-                            class="white--text align-end"
-                            height="200px"
-                            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                        >
-                            <v-card-title>{{abook.name}}</v-card-title>
-                        </v-img>
-
-                        <v-card-actions>
-                            <v-btn
-                                color="orange"
-                                text
-                            >
-                                Play
-                            </v-btn>
-
-                            <v-btn
-                                color="orange"
-                                text
-                            >
-                                Details
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
+                  <AudioBookCard :abook="abook"></AudioBookCard>
                 </v-col>
+                <v-responsive
+                    v-if="n === 2"
+                    :key="`width-${n}`"
+                    width="100%"
+                ></v-responsive>
             </v-row>
         </v-container>
     </div>
@@ -47,15 +30,16 @@
 <script lang="ts">
     import {AudioBookDTO} from 'ablams-js-dto/src/domain/models';
     import {Component, Vue} from 'vue-property-decorator';
-    import AudioLibraryResource from '@/rest/AudioLibraryResource';
     import AudioBookResource from "@/rest/AudioBookResource";
+    import AudiobookCard from "@/components/AudiobookCard.vue";
 
-    @Component
+
+    @Component({
+        components: {AudioBookCard: AudiobookCard}
+    })
     export default class AudioBooksView extends Vue {
 
         private aBooks: AudioBookDTO[] = [];
-
-
 
         protected created(): void {
             this.initialize();
@@ -67,8 +51,6 @@
                 this.aBooks = value.data;
             });
         }
-
-
     }
 </script>
 

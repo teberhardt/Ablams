766175eab14a120a3aab00a99cb0779d91aef774
teberhardt <teberhardt@mail.de>
2020-06-02 02:28:46 +0200

    add audiofile with pageable link in RootController

--- ablams-backend/src/main/java/de/teberhardt/ablams/web/rest/controller/RootController.java
+++ ablams-backend/src/main/java/de/teberhardt/ablams/web/rest/controller/RootController.java
@@ -1,6 +1,5 @@
 package de.teberhardt.ablams.web.rest.controller;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +14,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class RootController {
 
     @GetMapping
-    ResponseEntity<RepresentationModel> root() {
+    ResponseEntity<RepresentationModel<?>> root() {
 
-        RepresentationModel model = new RepresentationModel();
+        RepresentationModel<?> model = new RepresentationModel<RepresentationModel<?>>();
 
         model.add(linkTo(methodOn(RootController.class).root()).withSelfRel());
         model.add(linkTo(methodOn(AudiobookController.class).getAllAudiobooks(null)).withRel("audio-books"));
-        model.add(linkTo(methodOn(AudiofileController.class).getAllAudiofiles()).withRel("audio-files"));
+        model.add(linkTo(methodOn(AudiofileController.class).getAllAudiofiles(null)).withRel("audio-files"));
         model.add(linkTo(methodOn(AudioLibraryController.class).getAllAudioLibraries()).withRel("audio-libraries"));
         model.add(linkTo(methodOn(AudioSeriesController.class).getAllAudioSeries()).withRel("audio-series"));
         model.add(linkTo(methodOn(AuthorController.class).getAllAuthors()).withRel("authors"));
